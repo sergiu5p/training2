b3fbe73d906c9a02238db3dfdb4cac72bfe852ba
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-26 16:52:19 +0300

    Use the session

--- app/Product.php
+++ app/Product.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use Illuminate\Contracts\Session\Session;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function __construct()
+    {
+        Session::push('cart', []);
+    }
 }
