49cd38d985839d4023ec12f8be50fb2c1b59cb5a
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-26 16:52:44 +0300

    Show the products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,7 +7,10 @@
     @foreach($products as $product)
         <div>
             <img src={{ "/images/".$product->id.".".$product->image_extension }} alt="{{ $product->title }}" width="150" height="150">
-
+            <h4>{{ $product->title }}</h4>
+            <p>{{ $product->description }}</p>
+            <h4>{{ $product->price }}</h4>
+            <a href="#">{{ trans("Add") }}</a>
         </div>
     @endforeach
 
