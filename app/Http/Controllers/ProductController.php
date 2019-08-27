1c27f9f9b8d00e6f559050de2413f604543d4b74
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-27 15:28:24 +0300

    Create the session.. unstable

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Cart;
 use App\Product;
-use Illuminate\Contracts\Session\Session;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
 class ProductController extends Controller
 {
@@ -16,7 +17,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::whereNotIn(Session::get('cart'));
+        $products = Product::all();
         return view('products.index', compact('products'));
     }
 
@@ -36,9 +37,17 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $id)
     {
-        //
+        $product = Product::find($id);
+        $oldCart = Session::has('cart') ? Session::get('cart') : null;
+        $cart = new Cart($oldCart);
+        $cart->add($product, $product->id);
+
+        $request->session()->put('cart', $cart);
+
+//        dd($request->session()->get('cart'));
+        return redirect()->route('product.index');
     }
 
     /**
