bab6e6048f9eb5a301884d32b3021c96782078d8
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-28 17:03:38 +0300

    Try to figure out how to fetch information into email message

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\mailOrder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class OrderController extends Controller
 {
-    public function cart($id)
+    public function sendMail(Request $request)
     {
-
+        Mail::to('saquib.rizwan@cloudways.com')->send(
+            new mailOrder($request->all())
+        );
     }
 }
