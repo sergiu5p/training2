a425b376deee7d84652677e34bf8a27ed4e08c49
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-29 13:48:31 +0300

    Create sendMail class

---
+++ app/Mail/sendMail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class sendMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $name;
+    public $email;
+    public $comments;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($name, $email, $comments)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->comments = $comments;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $name = $this->name;
+        $email = $this->email;
+        $comments = $this->comments;
+        return $this->markdown('email.message', compact('name', 'email', 'comments'));
+    }
+}
