5ac038b87e95b4ad373d468d84ff3effc3f4cf19
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-29 13:31:35 +0300

    It works, I need to optimize that, and add images to the email, and products

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,15 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Mail\mailOrder;
+use App\Mail\sendMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+
 
 class OrderController extends Controller
 {
     public function sendMail(Request $request)
     {
-        Mail::to('saquib.rizwan@cloudways.com')->send(
-            new mailOrder($request->all())
+        $this->validate($request, [
+            "name" => "required",
+            "email" => "required",
+            "comments" => "required"
+        ]);
+
+        $name = $request->name;
+        $email = $request->email;
+        $comments = $request->comments;
+
+        Mail::to('purcariu.sergiu@gmail.com')->send(
+            new sendMail($name, $email, $comments)
         );
     }
 }
