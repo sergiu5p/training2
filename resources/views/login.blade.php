2d1c9547380d0d7f328122b627b6b58095a08135
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-30 15:11:50 +0300

    Create Login page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,21 @@
+@extends('layout')
+
+@section('title', 'login')
+
+@section('content')
+
+    @if (isset($message))
+        <div>
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <form method="POST">
+        @csrf
+
+        <input type="text" name="username" placeholder="{{ trans('username') }}">
+        <input type="password" name="password" placeholder="{{ trans('password') }}">
+        <input type="submit" value="{{ trans('Login') }}">
+    </form>
+
+@endsection
