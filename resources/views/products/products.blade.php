e58e7e1c6fedb6d17d39d416a3b3e365521c81e7
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-08-30 15:57:46 +0300

    Create products view

---
+++ resources/views/products/products.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+
+@section('title', 'products')
+
+
+@section('content')
+
+    @foreach($products as $product)
+        <div>
+            <img src={{ "/images/".$product->id.".".$product->image_extension }} alt="{{ $product->title }}" width="150" height="150">
+            <h4>{{ $product->title }}</h4>
+            <p>{{ $product->description }}</p>
+            <h4>{{ $product->price }}</h4>
+            <a href="remove/{{$product->id}}">{{ trans("Remove") }}</a>
+        </div>
+    @endforeach
+
+@endsection
