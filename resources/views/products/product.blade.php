ffd2a0119d507dd9109f3c2c4709381bda86ce04
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-09-02 15:19:34 +0300

    Show the errors message if is the case

--- resources/views/products/product.blade.php
+++ resources/views/products/product.blade.php
@@ -24,4 +24,14 @@
         </form>
     </div>
 
+    @if ($errors->any())
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
 @endsection
