77d5506d150a96278a3a0d860ac49959d9f475d0
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-09-02 13:20:03 +0300

    Create the form

--- resources/views/products/product.blade.php
+++ resources/views/products/product.blade.php
@@ -4,6 +4,24 @@
 
 @section('content')
 
+    <div>
+        <form method="POST" action="{{route('update', $product->id)}}" enctype="multipart/form-data">
+            @csrf
 
+            Title: <input type="text" name="title" value="{{ $product->title }}" required>
+            <br>
+            <br>
+            Description: <input type="text" name="description" value="{{ $product->description }}" required>
+            <br>
+            <br>
+            Price: <input type="number" step="0.01" name="price" value="{{ $product->price }}" required>
+            <br>
+            <br>
+            <input type="file" name="image" placeholder="<?= trans("Image") ?>"  {{ $product->id == null ? "required" : "" }}  >
+            <br>
+            <br>
+            <input type="submit" name="save" value="<?= trans("Save") ?>">
+        </form>
+    </div>
 
 @endsection
