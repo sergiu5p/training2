1b011129b9c10ee1e99c3f715748bf1aab22dcde
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-09-03 16:15:14 +0300

    Show orders

--- resources/views/orders/orders.blade.php
+++ resources/views/orders/orders.blade.php
@@ -4,6 +4,15 @@
 
 @section('content')
 
-
+    @foreach ($orders as $order)
+        <div class="order">
+            <h4>{{ trans("Name: ").$order->name }}</h4>
+            <h4>{{ trans("E-mail: ").$order->email }}</h4>
+            <h4>{{ trans("Comments: ").$order->comments }}</h4>
+            <h4>{{ trans("Summed price: ").strval($order->summed_price) }} $</h4>
+            <h4>{{ trans("Creation date: ").$order->created_at }}</h4>
+            <a href="#">{{ trans("View") }}</a>
+        </div>
+    @endforeach
 
 @endsection
