86d39399af52e1642b90e13824fc10cd01a025a8
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-09-03 11:36:55 +0300

    Make a href list to all pages

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,13 +2,30 @@
 <html lang="en">
 
 <head>
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/style.css') }}" />
     <title></title>
 </head>
 
 <body>
-<div>
-    @yield('content')
-</div>
+    <ul>
+        @if (request()->session()->has('login'))
+            <li><a href={{ route('logout') }}>{{ trans('Logout') }}</a></li>
+            <li><a href={{ route('product.products') }}>products.php</a></li>
+        @else
+            <li><a href={{ route('login') }}>{{ trans('Login') }}</a></li>
+        @endif
+
+        <li><a href={{ route('product.show') }}>index.php</a></li>
+
+        @if (request()->session()->has('cart') && request()->session()->get('cart')->items)
+            <li><a href={{ route('product.showCart') }}>{{ trans('Go to cart') }}</a></li>
+        @else
+            <li>{{ trans('Cart is empty') }}</li>
+        @endif
+    </ul>
+    <div>
+        @yield('content')
+    </div>
 </body>
 
 </html>
