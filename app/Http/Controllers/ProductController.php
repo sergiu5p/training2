7454e0a5bf6ceece68016ea52908493800feab0c
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-09-04 10:59:37 +0300

    Remove blank function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,16 +12,6 @@ class ProductController extends Controller
 {
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
