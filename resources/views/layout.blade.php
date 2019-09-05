8ad18982c2983ccf644d818512cfb6615c2926b5
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-09-05 17:12:42 +0300

    showOrders to orders.index, product.showCart to cart.index (routes)

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,7 +11,7 @@
         @if (request()->session()->has('login'))
             <li><a href={{ route('logout') }}>{{ trans('Logout') }}</a></li>
             <li><a href={{ route('product.products') }}>products.php</a></li>
-            <li><a href={{ route('showOrders') }}>orders.php</a></li>
+            <li><a href={{ route('orders.index') }}>orders.php</a></li>
         @else
             <li><a href={{ route('login') }}>{{ trans('Login') }}</a></li>
         @endif
@@ -19,7 +19,7 @@
         <li><a href={{ route('product.show') }}>index.php</a></li>
 
         @if (request()->session()->has('cart') && request()->session()->get('cart')->items)
-            <li><a href={{ route('product.showCart') }}>{{ trans('Go to cart') }}</a></li>
+            <li><a href={{ route('cart.index') }}>{{ trans('Go to cart') }}</a></li>
         @else
             <li>{{ trans('Cart is empty') }}</li>
         @endif
