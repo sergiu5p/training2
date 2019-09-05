55fd01e5ccf4523632317bfcc9697bd629dc5f72
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-09-05 13:32:16 +0300

    $request->ajax() in index method

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Cart;
+use App\Product;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function index(Request $request)
+    {
+        if ($request->session()->has('cart')) {
+            $products = Product::query()
+                ->whereIn('id', data_get($request->session()->get('cart'), 'items'))
+                ->get();
+
+            if ($request->ajax()) {
+                return $products;
+            } else {
+                return view('products.cart', compact('products'));
+            }
+        }
+        if ($request->ajax()) {
+            return [];
+        } else {
+            return redirect()->route('product.show');
+        }
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all('product_id');
+        $product = Product::query()->findOrFail($data['product_id']);
+        $oldCart = $request->session()->has('cart') ? $request->session()->get('cart') : null;
+        $cart = new Cart($oldCart);
+        $cart->add($product->id);
+        $request->session()->put('cart', $cart);
+
+        return redirect()->route('product.show');
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        $index = array_search($id, data_get($request->session()->get('cart'), 'items'));
+
+        if ($index !== false) {
+            unset($request->session()->get('cart')->items[$index]);
+        }
+        return back();
+    }
+}
